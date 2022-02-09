51911d5100c0ce22ee3486f2bdc3639d16e1a5d3
Kwangsu-CHOI <wisecks@gmail.com>
2022-02-09 14:16:29 +1100

    sign in with email/pass, finishing handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import './sign-in.styles.scss';
 
@@ -17,16 +17,24 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: '', password: ''})
-  }
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
+    
+  };
 
   handleChange = event => {
     const { value, name } = event.target;
     this.setState({ [name]: value })
-  }
+  };
 
   render() {
     return (
@@ -34,8 +42,20 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <FormInput type="email" name="email" value={this.state.email} handleChange={this.handleChange} label='email' required />
-          <FormInput type="password" name="password" value={this.state.password} handleChange={this.handleChange} label='password' required />
+          <FormInput 
+            type="email" 
+            name="email" 
+            value={this.state.email} 
+            handleChange={this.handleChange} 
+            label='email' 
+            required />
+          <FormInput 
+            type="password" 
+            name="password" 
+            value={this.state.password} 
+            handleChange={this.handleChange} 
+            label='password' 
+            required />
 
           <div className="buttons">
             <CustomButton type="submit"> SIGN IN </CustomButton>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+
 const config = {
   apiKey: "AIzaSyB3fG351oy4zKPV2vNylbdJ_k2238IJ7-c",
   authDomain: "ks-ecomm.firebaseapp.com",
@@ -10,6 +11,31 @@ const config = {
   messagingSenderId: "281213632659",
   appId: "1:281213632659:web:468b994e9793d96c15cd66"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+}
 
 firebase.initializeApp(config);
 
